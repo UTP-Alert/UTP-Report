68b6ea895ae4b3d871e8b21df69a7ca08696ed6e
dev <126830375+DevCodeSoft@users.noreply.github.com>
2025-09-30 18:13:13 -0500

    agregando los servico de registro y login y dejando listo para crear los endpoints

--- utp-reporta-backend/src/main/java/com/utp_reporta_backend/service/impl/AuthServiceImpl.java
+++ utp-reporta-backend/src/main/java/com/utp_reporta_backend/service/impl/AuthServiceImpl.java
@@ -1,24 +1,63 @@
 package com.utp_reporta_backend.service.impl;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.utp_reporta_backend.config.JwtTokenProvider;
 import com.utp_reporta_backend.dto.LoginDTO;
 import com.utp_reporta_backend.dto.RegistroDTO;
+import com.utp_reporta_backend.enums.ERol;
+import com.utp_reporta_backend.model.Rol;
+import com.utp_reporta_backend.model.Usuario;
+import com.utp_reporta_backend.repository.RolRepository;
+import com.utp_reporta_backend.repository.UsuarioRepository;
 import com.utp_reporta_backend.service.AuthService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
-public class AuthServiceImpl implements AuthService{
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+	private final AuthenticationManager authenticationManager;
+	private final UsuarioRepository usuarioRepository;
+	private final RolRepository rolRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@Override
 	public String login(LoginDTO loginDTO) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrCorreo(), loginDTO.getPassword()));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		return jwtTokenProvider.generateToken((UserDetails) authentication.getPrincipal());
 	}
 
 	@Override
 	public String registrar(RegistroDTO registroDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		if (usuarioRepository.existsByUsername(registroDTO.getUsername())) {
+			throw new RuntimeException("El nombre de usuario ya existe");
+		}
+		if (usuarioRepository.existsByCorreo(registroDTO.getCorreo())) {
+			throw new RuntimeException("El email ya está en uso");
+		}
+		Usuario usuario = new Usuario();
+		usuario.setUsername(registroDTO.getUsername());
+		usuario.setCorreo(registroDTO.getCorreo());
+		usuario.setPassword(passwordEncoder.encode(registroDTO.getPassword()));
+		Rol rol = rolRepository.findByNombre(ERol.ROLE_USUARIO)
+				.orElseThrow(() -> new RuntimeException("Rol no encontrado"));
+		usuario.getRoles().add(rol);
+		usuarioRepository.save(usuario);
+
+		return "Usuario registrado exitosamente";
+
 	}
 
 }
