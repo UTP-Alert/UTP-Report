eed6650487b2c1b60a18fcfe9da67a984693c1bb
Andres Noguera <170201975+PRJ-CIMSAG-AN@users.noreply.github.com>
2025-10-02 01:06:15 -0500

    Mejorando crear usuario el formulario

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'; // Import CommonModule
-import { InicioSesion } from './Login/inicio-sesion/inicio-sesion';
-import { Inicio as SuperAdminInicio } from './SuperAdmin/inicio/inicio';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, InicioSesion, SuperAdminInicio], // Add CommonModule here
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'front';
-  showLogin: boolean = false; // Start with SuperAdmin view for testing
-
-  toggleView() {
-    this.showLogin = !this.showLogin;
-  }
 }
