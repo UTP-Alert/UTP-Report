e1b4edb83f88b7bfc733f0e447ffe903e801b886
dev <126830375+DevCodeSoft@users.noreply.github.com>
2025-10-02 03:22:57 -0500

    corriguiendo el cors

--- utp-reporta-backend/src/main/java/com/utp_reporta_backend/filter/CORSFilter.java
+++ utp-reporta-backend/src/main/java/com/utp_reporta_backend/filter/CORSFilter.java
@@ -24,7 +24,7 @@ public class CORSFilter implements Filter{
 		HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200/");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
